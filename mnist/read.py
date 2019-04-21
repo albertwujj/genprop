43758cbb9ad641be8ea1f7f3fb4a738990c0f405
Albert Wu <albertwujj@gmail.com>
2019-04-20 21:51:31 -0700

    x

--- mnist/read.py
+++ mnist/read.py
@@ -6,6 +6,6 @@ def read_mnist():
         with open(filename, 'rb') as f:
             zero, data_type, dims = struct.unpack('>HBB', f.read(4))
             shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
-            return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
+            return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape).copy()
     fnames = ('train-images-idx3-ubyte','train-labels-idx1-ubyte','t10k-images-idx3-ubyte','t10k-labels-idx1-ubyte')
     return (read_idx('mnist/'+fname) for fname in fnames)
